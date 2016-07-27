09265900aec87d222a30781916ab93b4e1b61773
jennyleeweb <lee.jenny018@gmail.com>
2016-07-27 15:23:53 -0700

    Added photos for PhotoGallery

--- Week5/MyWebsite/scripts.js
+++ Week5/MyWebsite/scripts.js
@@ -2,5 +2,13 @@ window.onload = function() {
     alert('Welcome to my site (:');
 }
 
-$(".homepic").fadein();
 
+// $(".homepic").fadeIn("slow", "swing");
+// $(".homepic").fadeOut("slow", "swing");
+// $(".homepic").fadeToggle("slow");
+
+$('.homepic').click(function(){
+    $("#homepic1").fadeIn();
+    $("#homepic2").fadeIn("slow");
+    $("#homepic3").fadeIn(3000);
+});
